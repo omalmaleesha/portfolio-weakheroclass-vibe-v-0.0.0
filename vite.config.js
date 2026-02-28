ecfdc56643c81072fcbd6d23b0aa1a16b42cf95c
omalmaleesha <omalmaleesha03@gmail.com>
2026-02-28 22:18:07 +0530

    fix: update base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/portfolio-weakheroclass-vibe-v-0.0.0/",
   plugins: [ tailwindcss(),react()],
 })
