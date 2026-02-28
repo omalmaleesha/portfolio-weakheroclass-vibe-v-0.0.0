9ad915702f5f2c7a3b79aa41a118f58fa9a4fe7c
omalmaleesha <omalmaleesha03@gmail.com>
2026-02-28 23:47:21 +0530

    feat: add GitHub Actions workflow for deploying to GitHub Pages and update Vite base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio-weakheroclass-vibe-v-0.0.0/',
+  base: './',
   plugins: [ tailwindcss(),react()],
 })
